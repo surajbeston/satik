f28ffba6a885b6b31589945753d1774ea73c265b
shiva raj badu <shivabadu5@gmail.com>
2024-03-28 20:30:06 +0545

    website builder-grapesjs added

---
+++ app/src/components/WebBuilder.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <div id="gjs"></div>
+    </div>
+</template>
+
+<script setup>
+import { onMounted,ref } from 'vue'
+import grapesjs from 'grapesjs'
+import grapesjsblocks from 'grapesjs-blocks-basic';
+import webpage from 'grapesjs-preset-webpage';
+import 'grapesjs/dist/css/grapes.min.css'   
+
+
+const editor = ref(null);
+
+
+onMounted(() => {
+  
+
+   editor.value = grapesjs.init({
+     container: '#gjs',
+     height: '900px',
+     width: '1000px',
+     plugins: [grapesjsblocks,webpage],
+    
+   })
+  
+
+})
+</script>
+
+<style  >
+
+.gjs-pn-devices-c{
+
+  left: 0;
+
+}
+</style>
