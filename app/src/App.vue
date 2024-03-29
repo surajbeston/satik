9fe5adf549b4b0c5b09ec329d15d7e04d61b9eec
shiva raj badu <shivabadu5@gmail.com>
2024-03-29 23:02:44 +0545

    web3 storage and improve of web builder

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,25 +1,18 @@
 <script setup>
-import WebBuilder from './components/WebBuilder.vue';
+import WebBuilder from "./components/WebBuilder.vue";
 </script>
 
 <template>
-  <div>
-   
-    <WebBuilder/>
+  <div class="wrapper">
+    <WebBuilder />
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.wrapper {
+  max-width: 1400px;
+  margin: 0 auto;
+  width: 95%;
+  background: #4e4e4e;
 }
 </style>
--- app/src/components/WebBuilder.vue
+++ app/src/components/WebBuilder.vue
@@ -1,40 +1,61 @@
 <template>
-    <div>
-      <div id="gjs"></div>
-    </div>
+  <div class="editor-container">
+    <div style="width: 100%" id="gjs"></div>
+    <button @click="getCode">Get Code</button>
+  </div>
 </template>
 
 <script setup>
-import { onMounted,ref } from 'vue'
-import grapesjs from 'grapesjs'
-import grapesjsblocks from 'grapesjs-blocks-basic';
-import webpage from 'grapesjs-preset-webpage';
-import 'grapesjs/dist/css/grapes.min.css'   
+import { onMounted, ref } from "vue";
+// grapes and grapes plugins
+import grapesjs from "grapesjs";
+import grapesjsblocks from "grapesjs-blocks-basic";
+import webpage from "grapesjs-preset-webpage";
+import grjNavbar from "grapesjs-navbar";
 
+import "grapesjs/dist/css/grapes.min.css";
+// web3 storage
+import { createClient } from "/home/badu/workspace/satik/app/src/client.js";
 
 const editor = ref(null);
 
-
 onMounted(() => {
-  
+  editor.value = grapesjs.init({
+    container: "#gjs",
+    plugins: [grapesjsblocks, webpage, grjNavbar],
+  });
+});
+const getCode = async () => {
+  const html = editor.value.getHtml();
+  const cssCode = editor.value.getCss({ clean: true });
+  const jsCode = editor.value.getJs();
+  //   combine css to the html file
+  let code = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <style>
+  ${cssCode}
+  </style>
+</head>
 
-   editor.value = grapesjs.init({
-     container: '#gjs',
-     height: '900px',
-     width: '1000px',
-     plugins: [grapesjsblocks,webpage],
-    
-   })
-  
+  ${html}
+ 
+</html>`;
 
-})
+  const files = [new File([code], "index.html")];
+  await createClient(files);
+};
 </script>
 
-<style  >
-
-.gjs-pn-devices-c{
-
+<style>
+.gjs-pn-devices-c {
   left: 0;
-
+}
+.editor-container {
+  width: 1200px;
 }
 </style>
