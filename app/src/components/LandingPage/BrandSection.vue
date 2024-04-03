63f64c7980aab824b7fc91f49ca46c471ddeb645
shiva raj badu <shivabadu5@gmail.com>
2024-04-04 00:24:23 +0545

    brand section progress

--- app/src/components/LandingPage/BrandSection.vue
+++ app/src/components/LandingPage/BrandSection.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
+  <div class="relative py-8">
+    <DividerPattern />
+    <img class="w-12 h-12" src="../../assets/icons/branding.png" alt="brand" />
     <div class="flex items-center justify-between relative z-10">
       <h1 class="text-3xl font-inria-sans my-6 text-secondary-0 font-semibold">
-        Creators
+        Brands
       </h1>
       <RouterLink
         class="text-lg underline text-secondary-10 underline-offset-4 hover:text-secondary-30 font-medium duration-300"
@@ -13,6 +15,8 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import DividerPattern from "../DividerPattern.vue";
+</script>
 
 <style scoped></style>
