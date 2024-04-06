68f1ce2eb5e6a9718ad62a82febde20d012ac8b7
shiva raj badu <shivabadu5@gmail.com>
2024-04-06 21:39:56 +0545

    read more and influcer follower section added

--- app/src/pages/BrandProfile.vue
+++ app/src/pages/BrandProfile.vue
@@ -58,7 +58,7 @@
                 @click="
                   $router.push(`/brand/${proposal.account.influencer.username}`)
                 "
-                class="w-full md:w-auto"
+                class="w-full md:w-auto min-w-[120px]"
               >
                 <img
                   class="w-full md:w-[140px] max-h-[200px] object-cover rounded-xl h-full"
@@ -88,23 +88,17 @@
                   {{ proposal.account.accepted ? "Accepted" : "Pending" }}
                 </p>
 
-                <p class="text-lg text-neutral-10 line-clamp-2 min-h-[50px]">
+                <p
+                  class="text-base font-normal text-neutral-10 line-clamp-2 min-h-[50px]"
+                >
                   {{ proposal.account.influencer.bio }}
                 </p>
-                <p class="text-xl font-medium text-neutral-20">
-                  Message:<span class="block ml-2">
-                    {{ proposal.account.message }}</span
-                  >
+                <p class="text-xl font-bold text-neutral-20">
+                  Message: <ReadMore :text="proposal.account.message" />
                 </p>
+
                 <p class="text-xl font-medium text-neutral-20">
                   Webpage:
-<<<<<<< HEAD
-                  <span class="block ml-2">{{ proposal.account.webpage }}</span>
-                </p>
-                <p class="text-xl font-medium text-neutral-20">
-                  Accepted:
-                  <span class="block ml-2">{{ proposal.account.accepted ? "Accepted" : "Not Accepted" }}</span>
-=======
                   <a
                     v-if="proposal.account.webpage"
                     class="text-secondary-0 underline underline-offset-4 ml-2"
@@ -112,11 +106,10 @@
                     target="_blank"
                     >Visit link</a
                   >
->>>>>>> 1b942a314a5bc9bce5473513ce4b6f1e82591505
                 </p>
                 <p class="text-xl font-medium text-neutral-20">
                   Sent date-time:
-                  <span class="font-bold text-lg block">{{
+                  <span class="font-bold text-base block">{{
                     formatDate(proposal.account.datetime.toString())
                   }}</span>
                 </p>
@@ -144,12 +137,12 @@
                       </p>
                       <p class="text-lg text-neutral-10">
                         Total Amount:<span class="font-bold ml-2">
-                          {{ product.account.totalAmount }}</span
+                          {{ product.account.totalAmount / 1000000 }}</span
                         >
                       </p>
                       <p class="text-lg text-neutral-10">
                         Influencer Amount:<span class="font-bold ml-2">
-                          {{ product.account.influencerAmount }}</span
+                          {{ product.account.influencerAmount / 1000000 }}</span
                         >
                       </p>
                     </div>
@@ -185,16 +178,19 @@ import {
   getBrandProposals,
   getProposalProducts,
 } from "../../anchor/utils";
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { useWallet } from "solana-wallets-vue";
 import { toast } from "vue3-toastify";
 import { formatDate } from "../helper/dateFormatter";
+import ReadMore from "../components/ReadMore.vue";
 const route = useRoute();
 const showProposals = ref(false);
 const proposals = ref([]);
 const publicKey = ref("");
 
 const brand = ref({});
+const isReadMore = ref(false);
+const message = ref("");
 
 const defaultProfile = ref("/loading.gif");
 const wallet = useWallet();
@@ -231,6 +227,12 @@ async function getProducts(proposal) {
   toast.info("Getting Products...");
   proposal.products = await getProposalProducts(proposal.publicKey.toBase58());
 }
+
+const displayMessage = computed(() => {
+  if (isReadMore.value) {
+    return;
+  }
+});
 </script>
 
 <style scoped></style>
