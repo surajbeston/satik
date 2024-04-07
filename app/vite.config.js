a4b9fe86c3dc9eb23435f5aaf79f633f239d8d63
shiva raj badu <shivabadu5@gmail.com>
2024-04-08 00:00:23 +0545

    dist folder

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,27 +1,29 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from 'path'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
-
+import path from "path";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), nodePolyfills({include: ['stream'], globals: {Buffer: true, process: true}})],
+  plugins: [
+    vue(),
+    nodePolyfills({
+      include: ["stream"],
+      globals: { Buffer: true, process: true },
+    }),
+  ],
   define: {
-    'process.env.ANCHOR_BROWSER': true
-	},
-	// resolve: {
-	// 	alias: {
-	// 		'@': path.resolve(__dirname, 'src'),
-	// 	}
-	// },
+    "process.env.ANCHOR_BROWSER": true,
+  },
+  // resolve: {
+  // 	alias: {
+  // 		'@': path.resolve(__dirname, 'src'),
+  // 	}
+  // },
 
-
-	// resolve: {
-	// 	alias: {
-	// 	  crypto: 'crypto-browserify',
-	// 	},
-	//   },
-
+  resolve: {
+    alias: {
+      crypto: "crypto-browserify",
+    },
+  },
 });
-
